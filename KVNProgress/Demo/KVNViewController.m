09909a65537658a672d84fec48046010ce8773d4
kevin-hirsch <kevin.hirsch.be@gmail.com>
2014-11-01 15:36:09 +0100

    Update demo and app icon

--- KVNProgress/Demo/KVNViewController.m
+++ KVNProgress/Demo/KVNViewController.m
@@ -10,9 +10,22 @@
 
 #import "KVNProgress.h"
 
+@interface KVNViewController ()
+
+@property (weak, nonatomic) IBOutlet UISwitch *fullscreenSwitch;
+
+@end
+
 @implementation KVNViewController
 
 #pragma mark - UIViewController
+
+- (void)viewDidLoad
+{
+	[super viewDidLoad];
+	
+	[UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+}
 
 - (void)viewDidAppear:(BOOL)animated
 {
@@ -25,123 +38,173 @@
 
 - (void)setupBaseKVNProgressUI
 {
-	[KVNProgress appearance].statusColor = [UIColor brownColor];
-	[KVNProgress appearance].circleStrokeForegroundColor = [UIColor brownColor];
-	[KVNProgress appearance].circleStrokeBackgroundColor = [[UIColor brownColor] colorWithAlphaComponent:0.3f];
+	// See the documentation of all appearance propoerties
+	[KVNProgress appearance].statusColor = [UIColor darkGrayColor];
+	[KVNProgress appearance].statusFont = [UIFont systemFontOfSize:17.0f];
+	[KVNProgress appearance].circleStrokeForegroundColor = [UIColor darkGrayColor];
+	[KVNProgress appearance].circleStrokeBackgroundColor = [[UIColor darkGrayColor] colorWithAlphaComponent:0.3f];
 	[KVNProgress appearance].circleFillBackgroundColor = [UIColor clearColor];
-	[KVNProgress appearance].backgroundFillColor = [UIColor colorWithRed:1.000 green:0.841 blue:0.582 alpha:0.900];
-	[KVNProgress appearance].backgroundTintColor = [UIColor colorWithRed:1.000 green:0.841 blue:0.582 alpha:1.000];
+	[KVNProgress appearance].backgroundFillColor = [UIColor colorWithWhite:0.9f alpha:0.9f];
+	[KVNProgress appearance].backgroundTintColor = [UIColor whiteColor];
+	[KVNProgress appearance].successColor = [UIColor darkGrayColor];
+	[KVNProgress appearance].errorColor = [UIColor darkGrayColor];
 	[KVNProgress appearance].circleSize = 75.0f;
 	[KVNProgress appearance].lineWidth = 2.0f;
 }
 
 - (void)setupCustomKVNProgressUI
 {
-	[KVNProgress appearance].statusColor = [UIColor darkGrayColor];
-	[KVNProgress appearance].circleStrokeForegroundColor = [UIColor colorWithRed:0.261 green:0.678 blue:0.199 alpha:1.000];
-	[KVNProgress appearance].circleStrokeBackgroundColor = [UIColor colorWithRed:0.559 green:0.834 blue:0.486 alpha:0.750];
-	[KVNProgress appearance].circleFillBackgroundColor = [[UIColor grayColor] colorWithAlphaComponent:0.25f];
-	[KVNProgress appearance].backgroundFillColor = [UIColor blueColor];
-	[KVNProgress appearance].backgroundTintColor = [UIColor colorWithRed:0.681 green:1.000 blue:0.582 alpha:1.000];
-	[KVNProgress appearance].circleSize = 150.0f;
-	[KVNProgress appearance].lineWidth = 5.0f;
+	// See the documentation of all appearance propoerties
+	[KVNProgress appearance].statusColor = [UIColor whiteColor];
+	[KVNProgress appearance].statusFont = [UIFont fontWithName:@"HelveticaNeue-Thin" size:15.0f];
+	[KVNProgress appearance].circleStrokeForegroundColor = [UIColor whiteColor];
+	[KVNProgress appearance].circleStrokeBackgroundColor = [UIColor colorWithWhite:1.0f alpha:0.3f];
+	[KVNProgress appearance].circleFillBackgroundColor = [UIColor colorWithWhite:1.0f alpha:0.1f];
+	[KVNProgress appearance].backgroundFillColor = [UIColor colorWithRed:0.173f green:0.263f blue:0.856f alpha:0.9f];
+	[KVNProgress appearance].backgroundTintColor = [UIColor colorWithRed:0.173f green:0.263f blue:0.856f alpha:1.0f];
+	[KVNProgress appearance].successColor = [UIColor whiteColor];
+	[KVNProgress appearance].errorColor = [UIColor whiteColor];
+	[KVNProgress appearance].circleSize = 110.0f;
+	[KVNProgress appearance].lineWidth = 1.0f;
 }
 
-#pragma mark - Actions
+#pragma mark - Predefined HUD's
 
 - (IBAction)show
 {
-	[KVNProgress show];
+	if ([self isFullScreen]) {
+		[KVNProgress showWithParameters:@{KVNProgressViewParameterFullScreen: @(YES)}];
+	} else {
+		[KVNProgress show];
+	}
 	
 	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
 		[KVNProgress dismiss];
-	});
-}
-
-- (IBAction)showWithStatus
-{
-	[KVNProgress showWithStatus:@"Loading..."];
-	
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[self showProgressFullScreen];
 	});
 }
 
 - (IBAction)showWithSolidBackground
 {
 	[KVNProgress showWithParameters:@{KVNProgressViewParameterStatus: @"Loading...",
-									  KVNProgressViewParameterBackgroundType: @(KVNProgressBackgroundTypeBlurred)}];
+									  KVNProgressViewParameterBackgroundType: @(KVNProgressBackgroundTypeSolid),
+									  KVNProgressViewParameterFullScreen: @([self isFullScreen])}];
 	
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+	dispatch_main_after(3.0f, ^{
 		[KVNProgress dismiss];
 	});
 }
 
-- (void)showProgressFullScreen
+- (IBAction)showWithStatus
 {
-	[KVNProgress showProgress:0.0f
-				   parameters:@{KVNProgressViewParameterStatus: @"Loading with progress...",
-								KVNProgressViewParameterBackgroundType: @(KVNProgressBackgroundTypeBlurred),
-								KVNProgressViewParameterFullScreen: @(YES)}];
-	[self updateProgress];
+	if ([self isFullScreen]) {
+		[KVNProgress showWithParameters:@{KVNProgressViewParameterStatus: @"Loading...",
+										  KVNProgressViewParameterFullScreen: @(YES)}];
+	} else {
+		[KVNProgress showWithStatus:@"Loading..."];
+	}
 	
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.70f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateStatus:@"You can change multiline status text dynamically !"];
-	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+	dispatch_main_after(3.0f, ^{
 		[KVNProgress dismiss];
 	});
 }
 
 - (IBAction)showProgress
 {
-	[KVNProgress showProgress:0.0f
-					   status:@"Loading with progress..."];
+	if ([self isFullScreen]) {
+		[KVNProgress showProgress:0.0f
+					   parameters:@{KVNProgressViewParameterStatus: @"Loading with progress...",
+									KVNProgressViewParameterFullScreen: @(YES)}];
+	} else {
+		[KVNProgress showProgress:0.0f
+						   status:@"Loading with progress..."];
+	}
+	
 	[self updateProgress];
 	
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.70f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateStatus:@"You can change multiline status text dynamically !"];
+	dispatch_main_after(2.7f, ^{
+		[KVNProgress updateStatus:@"You can change to a multiline status text dynamically!"];
 	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress dismiss];
+	dispatch_main_after(5.5f, ^{
+		[self showSuccess];
 	});
 }
 
-- (void)updateProgress
+- (IBAction)showSuccess
 {
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateProgress:0.3
-						   animated:YES];
-	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateProgress:0.5
-						   animated:YES];
-	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.8f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateProgress:0.6
-						   animated:YES];
-	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.7f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateProgress:0.93
-						   animated:YES];
-	});
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-		[KVNProgress updateProgress:1
-						   animated:YES];
-	});
+	if ([self isFullScreen]) {
+		[KVNProgress showSuccessWithParameters:@{KVNProgressViewParameterStatus: @"Success",
+												 KVNProgressViewParameterFullScreen: @(YES)}];
+	} else {
+		[KVNProgress showSuccessWithStatus:@"Success"];
+	}
+}
+
+- (IBAction)showError
+{
+	if ([self isFullScreen]) {
+		[KVNProgress showErrorWithParameters:@{KVNProgressViewParameterStatus: @"Error",
+											   KVNProgressViewParameterFullScreen: @(YES)}];
+	} else {
+		[KVNProgress showErrorWithStatus:@"Error"];
+	}
 }
 
 - (IBAction)showCustom
 {
 	[self setupCustomKVNProgressUI];
 	
-	[KVNProgress showProgress:0.0f
-					   status:@"You can custom several things : colors, fonts, circle size, and more !"];
+	if ([self isFullScreen]) {
+		[KVNProgress showProgress:0.0f
+					   parameters:@{KVNProgressViewParameterStatus: @"You can custom several things like colors, fonts, circle size, and more!",
+									KVNProgressViewParameterFullScreen: @(YES)}];
+	} else {
+		[KVNProgress showProgress:0.0f
+						   status:@"You can custom several things like colors, fonts, circle size, and more!"];
+	}
+	
 	[self updateProgress];
 	
-	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.5f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+	dispatch_main_after(5.5f, ^{
+		[self showSuccess];
 		[self setupBaseKVNProgressUI];
-		[KVNProgress dismiss];
+	});
+}
+
+#pragma mark - Helper
+
+- (void)updateProgress
+{
+	dispatch_main_after(2.0f, ^{
+		[KVNProgress updateProgress:0.3f
+						   animated:YES];
+	});
+	dispatch_main_after(2.5f, ^{
+		[KVNProgress updateProgress:0.5f
+						   animated:YES];
+	});
+	dispatch_main_after(2.8f, ^{
+		[KVNProgress updateProgress:0.6f
+						   animated:YES];
+	});
+	dispatch_main_after(3.7f, ^{
+		[KVNProgress updateProgress:0.93f
+						   animated:YES];
+	});
+	dispatch_main_after(5.0f, ^{
+		[KVNProgress updateProgress:1.0f
+						   animated:YES];
+	});
+}
+
+- (BOOL)isFullScreen
+{
+	return [self.fullscreenSwitch isOn];
+}
+
+static void dispatch_main_after(NSTimeInterval delay, void (^block)(void))
+{
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		block();
 	});
 }
 
